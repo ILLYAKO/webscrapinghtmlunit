fdc8e9164d2d2f9b56fad6e809ef95cb4ea51bd7
ILLYAKO <ilhaifa@yandex.ru>
2019-02-18 22:09:51 -0600

    json out print.

--- src/main/java/domain/Item.java
+++ src/main/java/domain/Item.java
@@ -3,7 +3,6 @@ import java.math.BigDecimal;
 
 public class Item {
     private String title;
-    private BigDecimal price;
     private String url;
 
     public String getTitle() {
@@ -14,19 +13,11 @@ public class Item {
         this.title = title;
     }
 
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
     public String getUrl() {
         return url;
     }
 
     public void setUrl(String url) {
-        this.url = url;
+        this.url ="https://www.indeed.ca"+ url;
     }
 }
