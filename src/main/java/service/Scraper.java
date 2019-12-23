eeffc0eb10cc7b0a92a2d58a5facea160118ed47
ILLYAKO <ilhaifa@yandex.ru>
2019-12-22 23:23:04 -0600

    DESCTOPCONTROLLER

--- src/main/java/service/Scraper.java
+++ src/main/java/service/Scraper.java
@@ -28,17 +28,20 @@ public class Scraper {
 
         List<HtmlElement> items = new ArrayList<>();
 
-        for (int i = 0; i <= jobPages; i++) {
+        for (int i = 0; i <= jobPages+1; i++) {
             try {
                 if (baseUrlIsFirst) {
                     baseUrlIsFirst = false;
                 } else {
                     baseUrl = rootUrl + "&start=" + (2 * i) + "0";
                 }
-
+                System.out.println("baseUrl:"+ baseUrl);
                 HtmlPage page = client.getPage(baseUrl);
 
-                items.addAll(page.getByXPath("//div[ starts-with(@class, 'jobsearch-SerpJobCard')]"));
+//                items.addAll(page.getByXPath("//div[ starts-with(@class, 'jobsearch-SerpJobCard')]"));
+                items.addAll(page.getByXPath("//div[ starts-with(@class, 'jobsearch-SerpJobCard unifiedRow row result')]"));
+
+                System.out.println("items.size()-"+items.size());
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -50,27 +53,56 @@ public class Scraper {
            // System.out.println((items.size() + 1) + " items found.");
             try {
                 for (HtmlElement htmlItem : items) {
-                    HtmlElement xElement = (HtmlElement) htmlItem.getFirstByXPath("*");
-                    HtmlAnchor itemAnchor;
-                    if (xElement.getNodeName().equalsIgnoreCase("a")) {
-                        itemAnchor = htmlItem.getFirstByXPath("*");
-                    } else {
-                        itemAnchor = htmlItem.getFirstByXPath(".//h2/a");
+//                    System.out.println("htmlItem 1-"+htmlItem.getTextContent());
+                    List<HtmlElement> hItems= htmlItem.getElementsByTagName("div");
+
+
+//                    HtmlElement xElement = (HtmlElement) htmlItem.getFirstByXPath("*");
+//                    System.out.println("xElement: "+xElement.getNodeName());
+
+//                    HtmlAnchor itemAnchor;
+//                    HtmlElement itemDiv= hItems.get(0);
+                    HtmlElement itemDivTitle= htmlItem.getFirstByXPath(".//div[@class='title']");
+                    HtmlAnchor itemTitleAnchor = itemDivTitle.getFirstByXPath(".//a");
+
+
+//                    if (xElement.getNodeName().equalsIgnoreCase("a")) {
+//                        itemAnchor = htmlItem.getFirstByXPath("*");
+//                        System.out.println("--1");
+//                    } else {
+//                        itemAnchor = htmlItem.getFirstByXPath(".//h2/a");
+//                        System.out.println("--2");
+//                    }
+
+//                    String titleItemAnchor = itemAnchor.asText();
+                    String titleItem = itemDivTitle.asText();
+                    System.out.println("titleItem-"+titleItem);
+
+//                    HtmlSpan spanCompanyName = itemAnchor.getFirstByXPath("//div[@class='companyInfoWrapper']/div/span[@class='company']");
+//                    String companyName = spanCompanyName.asText();
+
+                    HtmlSpan spanCompanyName=htmlItem.getFirstByXPath(".//div[@class='sjcl']/div/span[@class='company']");
+                    String companyName = spanCompanyName.asText();
+//
+//                    HtmlSpan spanCity = itemAnchor.getFirstByXPath("//div[@class='companyInfoWrapper']/span[@class='location']");
+//                    String city = spanCity.asText();
+                    String city="";
+                    try{
+                        HtmlSpan spanCity = htmlItem.getFirstByXPath(".//div[@class='sjcl']/span[@class='location accessible-contrast-color-location']");
+                        city= spanCity.asText();
+                    }catch (NullPointerException e){
+                        HtmlDivision divCity = htmlItem.getFirstByXPath(".//div[@class='sjcl']/div[@class='location accessible-contrast-color-location']");
+                        city= divCity.asText();
                     }
 
-                    String titleItemAnchor = itemAnchor.asText();
-
-                    HtmlSpan spanCompanyName = itemAnchor.getFirstByXPath("//div[@class='companyInfoWrapper']/div/span[@class='company']");
-                    String companyName = spanCompanyName.asText();
-
-                    HtmlSpan spanCity = itemAnchor.getFirstByXPath("//div[@class='companyInfoWrapper']/span[@class='location']");
-                    String city = spanCity.asText();
-
-                    String urlItemAnchor = itemAnchor.getHrefAttribute();
+//
+//                    String urlItemAnchor = itemAnchor.getHrefAttribute();
+                    String urlItemAnchor = itemTitleAnchor.getHrefAttribute();
 
 
                     Item itemResult = new Item();
-                    itemResult.setTitle(titleItemAnchor);
+//                    itemResult.setTitle(titleItemAnchor);
+                    itemResult.setTitle(titleItem);
                     itemResult.setCompany(companyName);
                     itemResult.setCity(city);
                     itemResult.setUrl(urlItemAnchor);
@@ -79,7 +111,7 @@ public class Scraper {
 
                    ObjectMapper mapper = new ObjectMapper();
                     String jsonString = mapper.writeValueAsString(itemResult);
-                   //System.out.println(jsonString);
+                   System.out.println(jsonString);
                 }
             } catch (JsonProcessingException e) {
                 e.printStackTrace();
@@ -90,11 +122,16 @@ public class Scraper {
     private static int jobPageCounter(WebClient client, String baseUrl) {
         try {
             HtmlPage page = client.getPage(baseUrl);
-            HtmlDivision divSearchCount = page.getFirstByXPath("//div[@id='searchCount']");
-            String[] stringNumberJobs = divSearchCount.getFirstChild().asText().split(" ");
+//            HtmlDivision divSearchCount = page.getFirstByXPath("//div[@id='searchCount']");
+            HtmlDivision divSearchCountPages = page.getFirstByXPath("//div[@id='searchCountPages']");
+            System.out.println("divSearchCountPages:"+ divSearchCountPages.asText());
+
+            String[] stringNumberJobs = divSearchCountPages.getFirstChild().asText().split(" ");
             int numberJobs = Integer.parseInt(stringNumberJobs[3].replaceAll(",", ""));
+            System.out.println("numberJobs:"+numberJobs);
             return (numberJobs / 20);
         } catch (IOException e) {
+            System.out.println("jobPageCounter Exception!");
             e.printStackTrace();
             return 0;
         }
